3b9378abc32345ae76f1ac236eebf3d2707f9888
yc <yc2top@163.com>
2018-01-29 14:36:37 +0800

    some changes

---
+++ kafka/src/main/java/com/example/clusters/AppServer.java
@@ -0,0 +1,13 @@
+package com.example.clusters;
+
+import org.springframework.boot.SpringApplication;  
+import org.springframework.boot.autoconfigure.SpringBootApplication;  
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;  
+  
+@SpringBootApplication  
+@EnableDiscoveryClient  
+public class AppServer {  
+    public static void main(String[] args) {  
+        SpringApplication.run(AppServer.class, args);  
+    }  
+} 
